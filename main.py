7133c2c59b2b5e52b075d5a5696a882d2dd8f96b
Emma Nourry <82103105+emmanry@users.noreply.github.com>
2022-11-29 12:27:10 +0100

    print and comments

--- main.py
+++ main.py
@@ -10,14 +10,22 @@ words = []
 # Load all data
 words, occurencesInDoc, occurencesInCollection, occurencesInQueries = loadData.loadData("CISI/CISI.ALL", "CISI/CISI.QRY")
 
+print('words :', len(words))
+
 # Calculate IDF for each word of data
 wordsIDF = calculateIDF.calculateIDF(len(occurencesInDoc), occurencesInCollection)
+
+print('wordsIDF :', len(wordsIDF))
 
 # Calculate TFIDF for each word of data
 doc_TFIDF = calculateTFIDF_doc.calculateTFIDF_doc(wordsIDF, occurencesInDoc)
 
+print('doc :', len(doc_TFIDF['1']))
+
 # Calculate TF for each word of queries
 query_TFIDF = calculateTFIDF_query.calculateTFIDF_query(wordsIDF, occurencesInQueries)
+
+print('qry :', len(query_TFIDF['1']))
 
 scoreDocs_Q = scoreDocs.scoreDocs(query_TFIDF["2"], doc_TFIDF)
 
@@ -46,3 +54,20 @@ print(scoreDocs_Q.keys())
 
 # ScoreDocs returns : 
 # {id_doc : score ; ...} pour une requete
+
+# joueur : M B C
+# pred   : 6 8 3
+# res    : 8 0 2
+#
+# M -> 6 bonnes, B -> 0 et C-> 2 bonnes
+# P = 8 (nb bonnes) / 17 (nb total)
+# ce que je predis en tant que bon vs ce que je predis en mauvais
+# VP, FN vs FP, VN (vrai positif, vrai negatif = ce qui ne se passe pas)
+# V = vérité, ce qui s'est vrmt passé
+# Precision = VP / VP + FN 
+# Rappel = VP / VP + FP (est ce qu'il oublie bcp d'élément)
+# f mesure = ratio entre précision/ rappel
+# Q = prediction 10 
+# dans les 10 combien sont dans le V
+# si V contient 4 élém, 10 dans Q et 4 de bonne donc Précision = 4/10 (si il se trompe souvent) et Rappel = 4/4 (oublie aucun)
+# si V contient 17 élém, 10 dans Q et 4 de bonne donc Précision = 4/10 (si il se trompe souvent) et Rappel = 4/17 (oublie)
--- scoreDocs.py
+++ scoreDocs.py
@@ -5,10 +5,8 @@ def scoreDocs(query_TFIDF, doc_TFIDF):
     scoreDocs = {}
 
     for (idDoc, wordsTFIDF_doc) in doc_TFIDF.items():
-        score = distance.cosine(wordsTFIDF_doc, query_TFIDF[:len(wordsTFIDF_doc)])**2
-
-        scoreDocs[idDoc] = np.sqrt(score)
-
+        scoreDocs[idDoc] = distance.cosine(wordsTFIDF_doc, query_TFIDF[:len(wordsTFIDF_doc)])
+    
     scoreDocs_sorted = dict(sorted(scoreDocs.items(), key=lambda item : item[1])[:10])
 
     return scoreDocs_sorted
